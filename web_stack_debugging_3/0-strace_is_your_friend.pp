6cb94363491d49e8e1bbe0b385806788a7bf5d99
Walter <w.angoye@alustudent.com>
2023-04-01 02:40:08 +0300

    web debugging

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # puppet code to fix 'wp-setting.php'
+
 exec { '/var/www/html/wp-setting.php':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
